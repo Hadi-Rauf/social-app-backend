966c5a99bb19c987d9d15801e43d2a6a9c7b32ef
hadimohammad <mohammadhadirauf@gmail.com>
2022-02-23 18:48:50 +0500

    users api done

--- config/users.js
+++ config/users.js
@@ -1,24 +1,4 @@
-const moongose = require("mongoose");
-
-
-
-const Users = moongose.Schema({
-    name:{
-    type: String,
-    required: true
-    },
-
-    email : {
-        type: String,
-        required: true
-    },
-    password : {
-        type : String,
-        required: true
-    },
-    date : {
-        type : Date,
-        required: true,
+ 
         default: Date.now
     }
 })
